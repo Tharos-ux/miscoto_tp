ba2fc0db8ce2e1cb946dff3bdc78438d6cf43ab7
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-11-22 16:53:32 +0100

    venv creation & instructions

---
+++ create_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PY=python3.10
+
+$PY -m pip install --upgrade pip
+$PY -m pip install virtualenv
+$PY -m virtualenv .venv_miscoto
+source .venv_miscoto/bin/activate
+pip install wheel
+pip install miscoto
+pip install clyngor clyngor-with-clingo
